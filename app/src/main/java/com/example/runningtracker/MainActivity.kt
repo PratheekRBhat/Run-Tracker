db11375dd1e6b1b8b14bea79c1182e22cfb5da54
0martel <pratheek.bht@gmail.com>
2020-09-07 19:00:38 +0530

    Set up Dagger-Hilt

--- app/src/main/java/com/example/runningtracker/MainActivity.kt
+++ app/src/main/java/com/example/runningtracker/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.runningtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Dao
+import com.example.runningtracker.database.RunDAO
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
--- app/src/main/java/com/example/runningtracker/database/RunningDatabase.kt
+++ app/src/main/java/com/example/runningtracker/database/RunningDatabase.kt
@@ -1,11 +1,14 @@
 package com.example.runningtracker.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Run::class], version = 1)
+@Database(
+    entities = [Run::class], version = 1
+)
 @TypeConverters(Converter::class)
-abstract class RunningDatabase {
+abstract class RunningDatabase : RoomDatabase() {
 
     abstract fun getRunDao(): RunDAO
 }
