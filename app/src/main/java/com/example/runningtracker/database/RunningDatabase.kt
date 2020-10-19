91f37c2648ce0c50babc9e266f93461db9031d4e
0martel <pratheek.bht@gmail.com>
2020-10-19 21:12:30 +0530

    Updated notification functionalities

--- app/src/main/java/com/example/runningtracker/database/RunningDatabase.kt
+++ app/src/main/java/com/example/runningtracker/database/RunningDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(
-    entities = [Run::class], version = 1
+    entities = [Run::class], version = 1, exportSchema = false
 )
 @TypeConverters(Converter::class)
 abstract class RunningDatabase : RoomDatabase() {
